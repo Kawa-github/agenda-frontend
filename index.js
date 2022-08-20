4978598e4784967e7c0653bef096e7427fc4735a
Kawa-github <kawa.rfaragao@gmail.com>
2022-08-20 17:36:18 -0300

    cadastrando evento pela interface e listando.

---
+++ index.js
@@ -0,0 +1,11 @@
+                
+        //     let atividade = await $.get('http://localhost:8000/api/home');
+                
+        //     let eventos = $.map(atividade, function (a) {
+        //         let name_event = a.name_event
+        //         let date_activity = a.date_activity
+    
+        //     // let responseDate = moment(date_activity ).format('YYYY-MM-DD');
+        //     // console.log('title = '+ name_event + ' data: ' + responseDate)
+            
+        // });
